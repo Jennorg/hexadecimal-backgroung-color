a7a8b2be890b65048e33b9ea29dd1fbb53167274
Jenorg <jenfermz44@gmail.com>
2023-12-05 16:31:07 -0400

    Upgrade js

--- app.js
+++ app.js
@@ -1,39 +1,16 @@
-const body = document.body
 const colorText = document.getElementById('Color');
 const generator = document.getElementById('Generator')
 
 generator.addEventListener('click', () => {
+    let numbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','F'];
     let color = '#';
     let randomNumber;
 
     for(let i=0; i<6; i++){
-        randomNumber = Math.floor(Math.random()*16);
-        
-        switch(randomNumber){
-            case 10:
-                color += 'A';
-                break;
-            case 11: 
-                color += 'B';
-                break
-            case 12:
-                color += 'C';
-                break
-            case 13:
-                color += 'D';
-                break;
-            case 14:
-                color += 'E';
-                break
-            case 15:
-                color += 'F';
-                break;
-
-            default:
-                color += randomNumber;
-        }
+        randomNumber = Math.floor(Math.random()*15);
+        color += numbers[randomNumber]        
     }
     
     colorText.innerText = color;
-    body.style.backgroundColor = color;
+    document.body.style.backgroundColor = color;
 })
